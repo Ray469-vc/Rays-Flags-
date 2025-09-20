c1f7e2748662c36b96bf36c32d7338bf07b058ec
Ray469-vc <muhhamdtaha408@gmail.com>
2025-09-21 00:24:35 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function order(item) {
+  const discordUser = "rattyray1";
+  const invite = "https://discord.gg/YOUR_INVITE"; // replace with your invite
+  alert(`Order for ${item}!\n\nMessage Discord user: ${discordUser}\nOr join: ${invite}`);
+}
