6c669806cd25182d2a7f97be2bce5458bd854ee5
Ray469-vc <muhhamdtaha408@gmail.com>
2025-09-21 01:14:35 +0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function order(item) {
-  const discordUser = "rattyray1";
-  const invite = "https://discord.gg/YOUR_INVITE"; // replace with your invite
-  alert(`Order for ${item}!\n\nMessage Discord user: ${discordUser}\nOr join: ${invite}`);
-}
